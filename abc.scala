8d4d284c07f73ad80088dec03c93dc42073fbd03
Lauren Richie <laurenjrichie@gmail.com>
2020-05-12 16:07:10 -0600

    Update abc.scala

--- abc.scala
+++ abc.scala
@@ -1,7 +1,2 @@
 // Databricks notebook source
-<<<<<<< HEAD
-abc
-cccc
-=======
-1 from DB 2 from GH 3 from DB 4 from GH
->>>>>>> branch 'master' of https://github.com/laurenjrichie/test
+edited notebook
